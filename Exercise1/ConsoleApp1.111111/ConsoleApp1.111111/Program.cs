777ee312a0c5591c2ab5e435fe24b52153881203
fmi <fmi@fmi.uni-plovdiv.bg>
2024-04-09 16:11:41 +0300

    initial cocmmit

---
+++ Exercise1/ConsoleApp1.111111/ConsoleApp1.111111/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp1._111111
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
